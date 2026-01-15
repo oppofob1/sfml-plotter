aaa30da7c1bbd5c3e64eb7d7e4f561c64e0ee50c
İbrahim Aslan <ibrahimbook413@gmail.com>
2026-01-15 09:59:19 +0300

    negative values can be graphed too, new system for point-value mapping

--- src/main.cpp
+++ src/main.cpp
@@ -5,62 +5,46 @@
 #include <optional>
 #include <iostream>
 
-// function to map value from x to axis range
-float mapValue(float value, float inMin, float inMax, float outMin, float outMax)
-{   
-    //mapping formula
-    return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
-}
-
-// example function to estimate square root
-float sqrt_estimator(float a, int iterations)
-{
-    float x = a / 2; // the number to take the square root of
-
-    for (int i = 0; i < iterations; ++i)
-    {
-        x = 0.5f * (x + (a / x));
-
-    }
-
-    return x;
-}
 
 //draws points as connected line segments
 sf::VertexArray buildGraph(
     const std::vector<float>& x_vals,
     const std::vector<float>& y_vals)
-{   
-    //stores the min and max values for x and y in the input arrays
-    float xmin = *std::min_element(x_vals.begin(), x_vals.end());
-    float xmax = *std::max_element(x_vals.begin(), x_vals.end());
-    float ymin = *std::min_element(y_vals.begin(), y_vals.end());
-    float ymax = *std::max_element(y_vals.begin(), y_vals.end());
-
+{
     sf::VertexArray graph(sf::PrimitiveType::LineStrip);
 
-    //maps the input values to the axis range
-    for (size_t i = 0; i < x_vals.size(); ++i)
-    {
-        float sx = mapValue(x_vals[i], xmin, xmax, 200.f, 1720.f);
-        float sy = mapValue(y_vals[i], 0, ymax, 880.f, 200.f);
+    //new technique, scaleable graph
+    constexpr float SCALE = 100.f;
+    constexpr float ORIGIN_X = 960.f;
+    constexpr float ORIGIN_Y = 540.f;
 
-        graph.append({ {sx, sy}, sf::Color::White });
+    for (size_t i = 0; i < x_vals.size(); ++i)
+    {   
+        //graph based on the scale
+        float pointX = ORIGIN_X + x_vals[i] * SCALE;
+        float pointY = ORIGIN_Y - y_vals[i] * SCALE;
+
+        graph.append({{pointX, pointY}, sf::Color::White});
     }
 
     return graph;
 }
 
+
+
+
 //draws the axis as lines
 sf::VertexArray buildAxes()
-{
+{   
     sf::VertexArray axes(sf::PrimitiveType::Lines);
 
-    axes.append({ {200, 200}, sf::Color::Red });
-    axes.append({ {200, 880}, sf::Color::Red });
+    //y
+    axes.append({ {960, 0}, sf::Color::Red });
+    axes.append({ {960, 1080}, sf::Color::Red });
 
-    axes.append({ {200, 880}, sf::Color::Red });
-    axes.append({ {1720, 880}, sf::Color::Red });
+    //x
+    axes.append({ {0, 540}, sf::Color::Red });
+    axes.append({ {1920, 540}, sf::Color::Red });
 
     return axes;
 }
@@ -76,14 +60,15 @@ int main()
     std::vector<float> x_vals  = {};
     std::vector<float> y_vals  = {};
 
-    // //changeable function inside of y_vals.push_back
-    for (float i = 1; i < 100; i += 0.1f)
+    // a range to draw in, values bigger than this or 
+    // smaller than the negative of this will not be calculated
+    int drawRange = 300;
+
+    //changeable function inside of y_vals.push_back
+    for (float i = -drawRange; i < drawRange; i += 0.1f)
     {
         x_vals.push_back(i);
-
-        // y_vals.push_back(cos(i));
-
-        // y_vals.push_back(sqrt_estimator(i, 10));
+        y_vals.push_back(cos(i));
     }
 
 
